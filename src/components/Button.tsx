1b170aa891b12967bbbf7eea53a699fa6b7a678b
Yuhong Wang <wangyhyh2012@gmail.com>
2019-02-18 18:12:59 -0500

    Added button propTypes

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,12 +1,24 @@
 import React from 'react';
 
-export default class Button extends React.Component<any, any> {
+
+interface ButtonComponentProps {
+  buttonText: string,
+  onClickFunction: (event: React.MouseEvent<HTMLElement>) => void
+}
+
+
+/**
+ * A generic button component
+ * @param {string} buttonText texts to be diplayed on the button
+ * @param {(event: React.MouseEvent<HTMLElement>) => void} onClickFunction callback function when clicked
+ */
+export default class Button extends React.Component<ButtonComponentProps> {
 
   constructor(props: any) {
     super(props);
   }
 
-  public render() {
+  public render() : React.ReactNode {
     return (
       <button onClick={this.props.onClickFunction}>
         {this.props.buttonText}
