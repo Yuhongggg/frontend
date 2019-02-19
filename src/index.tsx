6c33ae4129db6cb7521f82e8180e6f1ad6f4e8ab
Yuhong Wang <wangyhyh2012@gmail.com>
2019-02-18 19:48:49 -0500

    Added temp login page

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './pages/App';
+import LoginPage from './pages/login/LoginPage';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<LoginPage />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
---
+++ src/pages/login/LoginPage.tsx
@@ -0,0 +1,54 @@
+import React from 'react'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import AppBar from 'material-ui/AppBar';
+import RaisedButton from 'material-ui/RaisedButton';
+import TextField from 'material-ui/TextField';
+
+interface LoginPageState {
+  username: string,
+  password: string
+}
+
+export default class LoginPage extends React.Component<any, LoginPageState> {
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      username: '',
+      password: ''
+    }
+  }
+
+  handleClick = (e:any) => {
+    return;
+  }
+  render() {
+    return (
+      <div>
+        <MuiThemeProvider>
+          <div>
+            <AppBar
+              title="Login"
+            />
+            <TextField
+              hintText="Enter your Username"
+              floatingLabelText="Username"
+              onChange={(_, newValue) => this.setState({ username: newValue })}
+            />
+            <br />
+            <TextField
+              type="password"
+              hintText="Enter your Password"
+              floatingLabelText="Password"
+              onChange={(_, newValue) => this.setState({ password: newValue })}
+            />
+            <br />
+            <RaisedButton label="Submit" primary={true} style={style} onClick={(event) => this.handleClick(event)} />
+          </div>
+        </MuiThemeProvider>
+      </div>
+    );
+  }
+}
+const style = {
+  margin: 15,
+};
